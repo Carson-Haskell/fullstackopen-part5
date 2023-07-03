a3797923850ca8020fd67c6e986d7537cd1b5309
Carson Haskell <carson.haskell@gmail.com>
2023-07-03 16:52:52 -0700

    complete exercise 5.1

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,24 +1,46 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import LoginForm from './components/LoginForm';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  console.log(user);
+
+  const handleLogin = async (username, password) => {
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  if (user === null) {
+    return (
+      <>
+        <h2>Login to Blog</h2>
+        <LoginForm handleLogin={handleLogin} />
+      </>
+    );
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+const LoginForm = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await handleLogin(username, password);
+    setUsername('');
+    setPassword('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        Username:
+        <input
+          type="text"
+          name="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        Password:
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
