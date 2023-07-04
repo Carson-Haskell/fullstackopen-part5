0e3803276d1936aec09644eca1ac54e1382fc1f5
Carson Haskell <carson.haskell@gmail.com>
2023-07-04 13:20:34 -0700

    complete exercise 5.6 and 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <div className="blog">
+      {blog.title}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'hide' : 'view'}
+      </button>
+      {showDetails && (
+        <div>
+          <p>Blog: {blog.url}</p>
+          <p>Likes: {blog.likes}</p>
+          <p>Author: {blog.author}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
